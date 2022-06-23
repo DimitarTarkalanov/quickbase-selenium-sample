010a261045ade6488162fcdf8b59bb271ab1aab9
Dimitar Tarkalanov <dimitar.tarkalanov@gmail.com>
2022-06-23 20:31:15 +0300

    create APIDocsNavMenu

---
+++ src/main/java/pages/apidocs/APIDocsNavMenu.java
@@ -0,0 +1,58 @@
+package pages.apidocs;
+
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.PageObjectException;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class APIDocsNavMenu {
+    public static final List<String> PROTOCOLS_ITEM_LIST = Arrays.asList("WebDriver Protocol", "Appium", "Mobile JSON Wire Protocol", "Chromium", "Firefox", "Sauce Labs", "Selenium Standalone", "JSON Wire Protocol");
+    private static final By NAV_MENU_LOCATOR = By.className("menu");
+    private static final By NAV_MENU_SECTION_LOCATOR = By.cssSelector(".theme-doc-sidebar-menu > .menu__list-item");
+    private static final By NAV_MENU_SECTION_LIST_ITEM_LOCATOR = By.cssSelector("ul > li > a");
+    private final WebDriver driver;
+    private final WebElement navMenu;
+
+    public APIDocsNavMenu(WebDriver driver) {
+        this.driver = driver;
+        this.navMenu = driver.findElement(NAV_MENU_LOCATOR);
+    }
+
+    private WebElement getSectionElement(Section section) {
+        List<WebElement> sections = navMenu.findElements(NAV_MENU_SECTION_LOCATOR);
+        for (WebElement sectionElement : sections) {
+            if (StringUtils.containsIgnoreCase(sectionElement.getText(), section.name())) {
+                return sectionElement;
+            }
+        }
+
+        throw new PageObjectException(String.format("Section [%s] not found!", section.name()));
+    }
+
+    public List<String> getSectionList(Section section) {
+        List<String> sectionList = new ArrayList<>();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement sectionElement = getSectionElement(section);
+        sectionElement.click();
+        List<WebElement> sectionItems = wait.until(ExpectedConditions.visibilityOfNestedElementsLocatedBy(sectionElement, NAV_MENU_SECTION_LIST_ITEM_LOCATOR));
+        sectionItems.forEach(e -> {
+            wait.until(ExpectedConditions.attributeToBe(e, "tabindex", "0"));
+            sectionList.add(e.getText());
+        });
+
+        return sectionList;
+    }
+
+    public enum Section {
+        INTRODUCTION, EXPECT, PROTOCOLS, BROWSER, ELEMENT, MOCK
+    }
+}
