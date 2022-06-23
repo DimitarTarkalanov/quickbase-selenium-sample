41c5587c376deb38a6ffdeab5f27b9cbe84ddacc
Dimitar Tarkalanov <dimitar.tarkalanov@gmail.com>
2022-06-23 22:14:59 +0300

    update PageObject: - add getHeading() - add allure steps

--- src/main/java/pages/PageObject.java
+++ src/main/java/pages/PageObject.java
@@ -1,5 +1,7 @@
 package pages;
 
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,6 +14,7 @@ public abstract class PageObject {
     private static final String URL_ATTRIBUTE = "URL";
     private static final String TITLE_ATTRIBUTE = "Title";
     private static final String PAGE_VALIDATION_MESSAGE = "The page %s is incorrect!%nExpected:[%s]%nBut found:[%s]";
+    private static final By PAGE_HEADING_LOCATOR = By.tagName("h1");
     private final WebDriver driver;
     private final String pageUrl;
     private final String pageTitle;
@@ -22,18 +25,20 @@ public abstract class PageObject {
         this.pageTitle = pageTitle;
     }
 
+    @Step("Validate page attributes")
     public void onPage() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         validatePageAttribute(URL_ATTRIBUTE, wait.until(ExpectedConditions.urlContains(pageUrl)), pageUrl, driver.getCurrentUrl());
         validatePageAttribute(TITLE_ATTRIBUTE, wait.until(ExpectedConditions.titleIs(pageTitle)), pageTitle, driver.getTitle());
     }
 
-    private void validatePageAttribute(String attribute, Boolean isAttributeValid, String expectedValue, String currentValue) {
+    private void validatePageAttribute(String attribute, boolean isAttributeValid, String expectedValue, String currentValue) {
         if (!isAttributeValid) {
             throw new PageObjectException(String.format(PAGE_VALIDATION_MESSAGE, attribute, expectedValue, currentValue));
         }
     }
 
+    @Step("Navigate to page")
     public void navigateTo() {
         this.driver.get(pageUrl);
     }
@@ -44,5 +49,9 @@ public abstract class PageObject {
 
     public NavBar getNavBar() {
         return new NavBar(driver);
+    }
+
+    public String getHeading() {
+        return driver.findElement(PAGE_HEADING_LOCATOR).getText();
     }
 }
